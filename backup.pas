4befb93285469191e44c7d30c4c59550a0d6834b
Perry van den Hondel <perry.vandenhondel@gmail.com>
2015-12-23 10:57:27 +0100

    Initial commit

---
+++ backup.pas
@@ -0,0 +1,21 @@
+//
+// Backup
+//
+// 164
+//
+
+
+program backup;
+
+
+{$MODE OBJFPC}			// Create a Free Pascal Object file.
+{$LONGSTRINGS ON}		// Compile all strings as Ansistring
+
+
+uses
+	DateUtils,
+	Process, 
+	SysUtils,
+	USupportLibrary;
+
+	
