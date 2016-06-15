a6b31e8ce83b89f8c20a8be229e696c51bcfe0ac
Perry van den Hondel <perry.vandenhondel@gmail.com>
2016-06-15 15:06:41 +0200

    First working version

--- backup.pas
+++ backup.pas
@@ -5,11 +5,14 @@
 //
 
 
+
 program backup;
+
 
 
 {$MODE OBJFPC}			// Create a Free Pascal Object file.
 {$LONGSTRINGS ON}		// Compile all strings as Ansistring
+
 
 
 uses
@@ -18,6 +21,7 @@ uses
 	SysUtils,
 	USupportLibrary;
 
+	
 
 const
 	CONF_NAME = 								'backup.conf';
@@ -25,11 +29,91 @@ const
 	BACKUP_TYPE_INCR = 							2;
 	EXTENSION_BACKUP = 							'.rar';
 	
+
 	
+type
+	RFolder = record
+		path: Ansistring;
+		//timeStamp: Longint;
+	end;
+	TFolder = Array of RFolder;
+
+
+
 var
 	pathPid: Ansistring;
 	pathConfig: Ansistring;
+	afr: TFolder;
 	
+	
+	
+procedure DeleteDirectory(const Name: string);
+var
+  F: TSearchRec;
+begin
+  if FindFirst(Name + '\*', faAnyFile, F) = 0 then begin
+    try
+      repeat
+        if (F.Attr and faDirectory <> 0) then begin
+          if (F.Name <> '.') and (F.Name <> '..') then begin
+            DeleteDirectory(Name + '\' + F.Name);
+          end;
+        end else begin
+          DeleteFile(Name + '\' + F.Name);
+        end;
+      until FindNext(F) <> 0;
+    finally
+      FindClose(F);
+    end;
+    RemoveDir(Name);
+  end;
+end; // of DeleteDirectory
+
+
+
+procedure KeepNewestFolders(const inThisDirectory: Ansistring; keepNumber: integer);
+var
+	sr: TSearchRec;
+	size: integer;
+	x: integer;
+	folderCount: integer;
+begin
+	WriteLn('The folder ', inThisDirectory, ' contains the following sub-folders:');
+	
+	if FindFirst(FixFolderAdd(inThisDirectory) + '*', faAnyFile and faDirectory, sr) = 0 then
+    begin
+		repeat
+			with sr do
+			begin
+				if ((sr.Attr and faDirectory) = faDirectory) and (sr.Name <> '.') and (sr.Name <> '..') then
+				begin
+					WriteLn(sr.Name, '      ', sr.Time);
+					
+					size := Length(afr);
+					SetLength(afr, size + 1);
+					afr[size].path := sr.Name;
+				end;
+			end;
+		until FindNext(sr)<>0;
+    end;
+	FindClose(sr);
+	
+	
+	folderCount := 0;
+	for x := (Length(afr) - 1) downto 0 do
+	begin
+		Inc(folderCount);
+		if folderCount > keepNumber then
+		begin
+			WriteLn(folderCount:3, '    ', FixFolderAdd(inThisDirectory) + afr[x].Path, ' DELETE');
+			DeleteDirectory(FixFolderAdd(inThisDirectory) + afr[x].Path);
+		end
+		else
+			WriteLn(folderCount:3, '    ', FixFolderAdd(inThisDirectory) + afr[x].Path, ' KEEPING');
+	end;
+end; // of KeepNewestFolders
+
+
 
 function CountSubDirectories(inThisDirectory: Ansistring): integer;
 //
@@ -61,10 +145,44 @@ begin
 end;
 
 
-procedure MakeBackup(setName: Ansistring; folderSource: Ansistring; folderBackupSetFullDate: Ansistring; backupType: integer);
+
+function ExecuteCommand(strCommand: Ansistring): integer;
+//
+//	Run a command using the CMD.EXE interpreter, 
+//	returns the error level as the result of this function.
+//
+var
+	p: TProcess;		// Uses Process
+begin
+	p := TProcess.Create(nil);
+	p.Executable := 'cmd.exe'; 
+    p.Parameters.Add('/c ' + strCommand);
+	// Options:
+	//	poWaitOnExit: //
+	//	poNoConsole:
+	//	poUsePipes: 
+	p.Options := [poWaitOnExit];
+	//p.Options := [poWaitOnExit, poNoConsole];
+	p.Execute;
+	
+	Result := p.ExitStatus;
+	p.Free;
+end; // of function RunCommand.
+
+
+
+
+
+function MakeBackup(setName: Ansistring; folderSource: Ansistring; folderBackupSetFullDate: Ansistring; backupType: integer): integer;
+//
+//	Make a backup and return the error level back to calling procedure.
+//
 var
 	pathBackupFile: Ansistring;
 	c: Ansistring;
+	rc: integer;
+	preAction: Ansistring;
+	postAction: Ansistring;
 begin
 	WriteLn;
 	
@@ -74,33 +192,66 @@ begin
 	WriteLn(#9, 'Set name             : ', setName);
 	WriteLn(#9, 'Folder source        : ', folderSource);
 	WriteLn(#9, 'File to backup to    : ', pathBackupFile);
-	WriteLn(#9, 'Type of Backup       : ', backupType);
+	if backupType = BACKUP_TYPE_FULL then
+		WriteLn(#9, 'Type of Backup       : FULL');
+		
+	if backupType = BACKUP_TYPE_INCR then
+		WriteLn(#9, 'Type of Backup       : INCREMENTAL');
+		
+	preAction := ReadSettingKey(pathConfig, setName, 'ActionsPre');
+	if Length(preAction) > 0 then
+	begin
+		WriteLn;
+		WriteLn('Run the pre backup actions: ', preAction);
+		WriteLn;
+		
+		RunCommand(preAction);
+	end;
 	
 	MakeFolderTree(pathBackupFile);
 	
 	c := 'rar.exe';
 	c := c + ' ';
-	c := c + 'a';
+	c := c + 'a'; // Action is Add
 	c := c + ' ';
-	c := c + '-r';
+	c := c + '-r'; // Recusive folders
 	c := c + ' ';
-	c := c + '-m0';
+	c := c + '-m0';  // 0=Store...5=max compression
 	c := c + ' ';
-	c := c + '-ep2';
+	c := c + '-ep2'; // Expand paths to the full length
 	c := c + ' ';
 	if backupType = BACKUP_TYPE_FULL then
-		c := c + '-ac'
+		c := c + '-ac' // Clear archive bit after compression.
 	else 
-		c := c + '-ao';
+		c := c + '-ao'; // Add file with archive bit on.
 	c := c + ' ';
 	c := c + EncloseDoubleQuote(pathBackupFile);
 	c := c + ' ';
 	c := c + EncloseDoubleQuote(FixFolderAdd(folderSource) + '*.*');
 	
 	WriteLn;
+	WriteLn('Running command line:');
+	WriteLn;
 	WriteLn(c);
 	WriteLn;
+	
+	rc := ExecuteCommand(c);
+	
+	WriteLn;
+	
+	postAction := ReadSettingKey(pathConfig, setName, 'ActionsPost');
+	if Length(postAction) > 0 then
+	begin
+		WriteLn;
+		WriteLn('Run the post backup actions: ', postAction);
+		WriteLn;
+		
+		RunCommand(postAction);
+	end;
+	
+	MakeBackup := rc;
 end;
+
 
 
 procedure SelectBackup(setName: Ansistring; folderSource: Ansistring; folderBackup: Ansistring; keepFull: integer; keepIncr: integer);
@@ -111,14 +262,15 @@ var
 	folderBackupSetIncrDate: Ansistring;
 	countFull: integer;
 	countIncr: integer;
+	rc: integer;
 begin
 	WriteLn;
 	WriteLn('SelectBackup():');
-	WriteLn(#9, 'Set name                 : ', setName);
-	WriteLn(#9, 'Folder source            : ', folderSource);
-	WriteLn(#9, 'Folder Backup            : ', folderBackup);
-	WriteLn(#9, 'Keep full backups        : ', keepFull);
-	WriteLn(#9, 'Keep incremental backups : ', keepIncr);
+	WriteLn('                  Set name : ', setName);
+	WriteLn('             Folder source : ', folderSource);
+	WriteLn('             Folder Backup : ', folderBackup);
+	WriteLn('         Keep full backups : ', keepFull);
+	WriteLn('  Keep incremental backups : ', keepIncr);
 	
 	folderBackupSetFull := FixFolderAdd(folderBackup) + FixFolderAdd(setName) + 'full';
 	MakeFolderTree(folderBackupSetFull);
@@ -126,14 +278,11 @@ begin
 	folderBackupSetIncr := FixFolderAdd(folderBackup) + FixFolderAdd(setName) + 'incr';
 	MakeFolderTree(folderBackupSetIncr);
 	
-	countFull := CountSubDirectories(folderBackupSetFull);
-	countIncr := CountSubDirectories(folderBackupSetIncr);
-
 	folderBackupSetFullDate := FixFolderAdd(folderBackupSetFull) + GetDateFs(false) + '-' + GetTimeFs();
 	folderBackupSetIncrDate := FixFolderAdd(folderBackupSetIncr) + GetDateFs(false) + '-' + GetTimeFs();
 	
+	countFull := CountSubDirectories(folderBackupSetFull);
 	WriteLn('Found full backups is ', folderBackupSetFull, ': ', countFull);
-	
 	if countFull = 0 then
 	begin
 		WriteLn('INFO: No backups found, first backup is a full.');
@@ -141,18 +290,39 @@ begin
 	end
 	else
 	begin
+		countIncr := CountSubDirectories(folderBackupSetIncr);
 		if countIncr < keepIncr Then
 		begin	
-			WriteLn('INFO: Need more incremental backups, have ', countIncr, ' and need ', keepIncr);
-			MakeBackup(setName, folderSource, folderBackupSetIncrDate, BACKUP_TYPE_INCR);
+			WriteLn('INFO: Need more incremental backups, have ', countIncr, ' incrememtals found and need ', keepIncr, ' incrememtal backups');
+			rc := MakeBackup(setName, folderSource, folderBackupSetIncrDate, BACKUP_TYPE_INCR);
+			if rc = 0 then
+				WriteLn('INFO: Incremental backup successfully!')
+			else
+				WriteLn('WARN: Incremental backup returned a code: ', rc);
 		end
 		else if countIncr >= keepIncr then
 		begin
 			WriteLn('INFO: I have enough incremental backups, make a full again');
-			MakeBackup(setName, folderSource, folderBackupSetFullDate, BACKUP_TYPE_FULL);
+			rc := MakeBackup(setName, folderSource, folderBackupSetFullDate, BACKUP_TYPE_FULL);
+			if rc = 0 then
+			begin
+				WriteLn('INFO: Full backup successfully!, delete all incremental backups.');
+				WriteLn('Delete the with all increamental backups: ' + folderBackupSetIncr);
+				DeleteDirectory(folderBackupSetIncr);
+				
+				countFull := CountSubDirectories(folderBackupSetFull);
+				if countFull > keepFull then
+				begin
+					WriteLn('The are ', countFull, ' full backups available, only need to keep ', keepFull, ', older backups can be deleted!');
+					KeepNewestFolders(folderBackupSetFull, keepFull);
+				end;
+			end
+			else
+				WriteLn('WARN: Full backup returned a code: ', rc);
 		end;
 	end;
 end;
+
 
 
 procedure ProcessSingleBackupSet(setName: Ansistring);
@@ -196,11 +366,6 @@ end;
 
 
 procedure ProgRun();
-{var
-	setList: Ansistring;
-	setArray: TStringArray;
-	x: integer;
-	}
 begin
 	if ParamCount() = 0 then
 	begin
@@ -211,18 +376,7 @@ begin
 	begin
 		ProcessSingleBackupSet(ParamStr(1));
 	end;
-	{ 
-	setList := ReadSettingKey(pathConfig, 'Settings', 'Sets');
-	
-	SetLength(setArray, 0);   
-	setArray := SplitString(setList, ',');
-	for x := 0 to High(setArray) do
-	begin
-		//WriteLn(x, ': ', #9, LogsArray[x]);
-		ProcessSingleBackupSet(setArray[x]);
-	end;
-	}
-	WriteLn('Number of sub directories of D:\TEMP: ', CountSubDirectories('D:\TEMP\'));
+	//WriteLn('Number of sub directories of D:\TEMP: ', CountSubDirectories('D:\TEMP\'));
 end;
 
 
@@ -231,4 +385,5 @@ begin
 	ProgInit();
 	ProgRun();
 	ProgDone();
+	//KeepNewestFolders('D:\BACKUPS\VM70AS006-EXPORT\full', 7);
 end.
